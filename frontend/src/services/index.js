76cf9e69ce8f0db0e14e93f28e4b62cc67dc5838
Milan <milan.patel@skyttus.com>
2025-10-20 22:57:45 +0530

    url

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://task-manager-backend-alpha-olive.vercel.app/api/',
+  baseURL: process.env.REACT_APP_API_URL  || 'https://task-manager-backend-alpha-olive.vercel.app/api/',
 });
 
 export const fetchTasks = async () => {
